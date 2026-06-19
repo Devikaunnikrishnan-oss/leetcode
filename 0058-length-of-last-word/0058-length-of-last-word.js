50848942b0009e4182a92d63eed3812717bedd90
Devikaunnikrishnan-oss <devika11unnikrishnan@gmail.com>
2026-06-19 19:40:59 +0530

    Time: 0 ms (100%), Space: 52.8 MB (89.13%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+
+    let arr = s.trim().split(" ");
+
+    let lastWord = arr[arr.length - 1];
+
+    return lastWord.length;
+
+};
